77bf25da4a017db32ffa7b368d431dd2b3d193d5
naman jain <nn.jain5272@gmail.com>
2024-04-09 19:07:22 +0530

    resolve a console error  related to local storage in withauth page

--- components/withauth.js
+++ components/withauth.js
@@ -1,26 +1,26 @@
-"use client"
+import { useEffect } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 
+const WithAuth = (Children) => {
+  return (props) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const proxy_auth_token = searchParams.get('proxy_auth_token');
 
-import { useSearchParams , useRouter } from "next/navigation";
-const  WithAuth =  (Children ) => {
-return (props) => {
-    const router  = useRouter()
-   const searchParams = useSearchParams()
-    const  proxy_auth_token =searchParams.get('proxy_auth_token') 
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        // Access localStorage only in the client-side context
+        if (proxy_auth_token) {
+          localStorage.setItem('proxy_token', proxy_auth_token);
+          router.replace('/org');
+        } else if (localStorage.getItem('proxy_token')) {
+          router.replace('/org');
+        }
+      }
+    }, [proxy_auth_token, router]);
 
-    if (proxy_auth_token) {
-      localStorage?.setItem('proxy_token', proxy_auth_token);
-      router.replace("/org");
-      return;
-    }
-
-    if (localStorage?.getItem('proxy_token')) {
-      router.replace("/org");
-      return;
-    }
-    return <Children /> ;
+    return <Children {...props} />;
   };
-}
+};
+
 export default WithAuth;
-
-
