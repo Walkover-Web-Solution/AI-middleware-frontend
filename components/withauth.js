04c400673d1677727ee09888e501fbe7d2fab76b
naman jain <nn.jain5272@gmail.com>
2024-04-13 17:21:36 +0530

    withauth page

--- components/withauth.js
+++ components/withauth.js
@@ -1,26 +1,25 @@
-import { useEffect } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+"use client"
 
-const WithAuth = (Children) => {
-  return (props) => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const proxy_auth_token = searchParams.get('proxy_auth_token');
 
-    useEffect(() => {
-      if (typeof window !== 'undefined') {
-        // Access localStorage only in the client-side context
-        if (proxy_auth_token) {
-          localStorage.setItem('proxy_token', proxy_auth_token);
-          router.replace('/org');
-        } else if (localStorage.getItem('proxy_token')) {
-          router.replace('/org');
-        }
-      }
-    }, [proxy_auth_token, router]);
+import { useSearchParams , useRouter } from "next/navigation";
+const  WithAuth =  (Children ) => {
+return (props) => {
+    const router  = useRouter()
+   const searchParams = useSearchParams()
+    const  proxy_auth_token =searchParams.get('proxy_auth_token') 
 
-    return <Children {...props} />;
+    if (proxy_auth_token) {
+      localStorage?.setItem('proxy_auth_token', proxy_auth_token);
+      router.replace("/bridges");
+      return;
+    }
+
+    if (localStorage?.getItem('proxy_auth_token')) {
+      router.replace("/bridges");
+      return;
+    }
+    return <Children /> ;
   };
-};
-
+}
 export default WithAuth;
+
