3f5f2275da27f55e6f572d1b9bf766f48ed2a3f2
naman jain <nn.jain5272@gmail.com>
2024-04-16 13:24:05 +0530

    api key save functionlity and propt lost bug

--- components/chat.js
+++ components/chat.js
@@ -140,69 +140,74 @@ console.log(localDataToSend , "localDataToSend")
 
   return (
     <>
-    <div className=" p:2 sm:p-6 justify-between flex flex-col h-[80vh] lg:w-[50vw] border rounded-xl w-full z-10">
-      <div
-        id="messages"
-        className="flex flex-col w-full space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch z-10"
-      >
-        {messages.map(message => (
-          <div
-            key={message.id}
-            className={`chat ${
-              message.sender === "user" ? "chat-end" : "chat-start"
-            }`}
-          >
-            <div className="chat-image avatar"></div>
-            <div className="chat-header">
-              {message.sender}
-              <time className="text-xs opacity-50 pl-2">{message.time}</time>
-            </div>
-            <div className="chat-bubble">{message.content}</div>
-          </div>
-        ))}
+      <div className="w-full flex justify-between items-center">
+        {/* <div className="label"> */}
+          <span className="label-text">Playground</span>
+          <button className="btn btn-sm m-5" onClick={UpdateBridge}>Update Bridge</button>
+        {/* </div> */}
       </div>
 
-      <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0 w-full z-10">
-        <div className="relative flex justify-start items-center w-full ">
-          <div className="form-control w-full">
-            <div className="input-group flex gap-2 w-full">
-              {localDataToSend && (dataToSend?.configuration?.type !== "completion" ) && (dataToSend?.configuration?.type !== "embedding" )  && (
-                <input
-                  type="text"
-                  placeholder="Type here"
-                  className="input input-bordered w-full"
-                  value={newMessage}
-                  onChange={e => setNewMessage(e.target.value)}
-                  onKeyPress={handleKeyPress}
-                />
-              )}
-              <button
-                className="btn"
-                onClick={handleSendMessage}
-                disabled={loading}
-              >
-                {loading ? (
-                  <span className="loading loading-dots loading-lg"></span>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="h-6 w-6 ml-2 transform rotate-90"
-                  >
-                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                  </svg>
+      <div className=" p:2 sm:p-6 justify-between flex flex-col h-[80vh] lg:w-[50vw] border rounded-xl w-full z-10">
+        <div
+          id="messages"
+          className="flex flex-col w-full space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch z-10"
+        >
+          {messages.map(message => (
+            <div
+              key={message.id}
+              className={`chat ${message.sender === "user" ? "chat-end" : "chat-start"
+                }`}
+            >
+              <div className="chat-image avatar"></div>
+              <div className="chat-header">
+                {message.sender}
+                <time className="text-xs opacity-50 pl-2">{message.time}</time>
+              </div>
+              <div className="chat-bubble">{message.content}</div>
+            </div>
+          ))}
+        </div>
+
+        <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0 w-full z-10">
+          <div className="relative flex justify-start items-center w-full ">
+            <div className="form-control w-full">
+              <div className="input-group flex gap-2 w-full">
+                {localDataToSend && (dataToSend?.configuration?.type !== "completion") && (dataToSend?.configuration?.type !== "embedding") && (
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    value={newMessage}
+                    onChange={e => setNewMessage(e.target.value)}
+                    onKeyPress={handleKeyPress}
+                  />
                 )}
-              </button>
+                <button
+                  className="btn"
+                  onClick={handleSendMessage}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <span className="loading loading-dots loading-lg"></span>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="h-6 w-6 ml-2 transform rotate-90"
+                    >
+                      <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
           </div>
+          {errorMessage && (
+            <div className="text-red-500 mt-2">{errorMessage}</div>
+          )}
         </div>
-        {errorMessage && (
-          <div className="text-red-500 mt-2">{errorMessage}</div>
-        )}
       </div>
-    </div>
-    <button className="btn float-end  m-5" onClick={UpdateBridge}>Update Bridge</button>
 
     </>
   );
