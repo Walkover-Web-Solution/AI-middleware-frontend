8c66b272efc7ed64e88109093d937bc515a41020
naman jain <nn.jain5272@gmail.com>
2024-04-18 10:31:02 +0530

    resolve error while making build file

--- components/chat.js
+++ components/chat.js
@@ -144,7 +144,7 @@ const removeUndefinedOrNull = (obj) => {
 
 
   const UpdateBridge = async()=> {
-    const updatedConfigration = removeUndefinedOrNull(localDataToSend.configuration)1
+    const updatedConfigration = removeUndefinedOrNull(localDataToSend.configuration)
       await updateBridge( {bridgeId :  params.id , dataToSend :  {configuration : updatedConfigration, service : localDataToSend.service , apikey : localDataToSend.apikey}})
       toast.success("Bridge is updated");
   }
