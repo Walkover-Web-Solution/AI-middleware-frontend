4c317e118647665e8c5d2177d64254e7b8eaaf07
Naman Jain <namanjain@Namans-Mac-mini.local>
2024-05-23 17:33:52 +0530

    added condition for intregration reducer

--- store/reducer/bridgeReducer.js
+++ store/reducer/bridgeReducer.js
@@ -63,7 +63,14 @@ export const bridgeReducer = createSlice({
     },
     integrationReducer: (state, action) => {
       const { dataToSend, id } = action.payload;
-      state.allBridgesMap[id].integrationData.flows.push(dataToSend)
+      let flows = state.allBridgesMap[id].integrationData.flows;
+      const index = flows.findIndex(flow => flow.id === dataToSend.id);
+
+      if (index !== -1) {
+        flows[index] = dataToSend;
+      } else {
+        flows.push(dataToSend);
+      }
     }
   },
 });
