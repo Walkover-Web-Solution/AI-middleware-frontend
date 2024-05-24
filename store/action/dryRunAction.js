32fb9ec48746f69e059ee253e3346d55e56f8bca
Naman Jain <namanjain@Namans-Mac-mini.local>
2024-05-24 14:51:16 +0530

    _

--- store/action/dryRunAction.js
+++ store/action/dryRunAction.js
@@ -4,22 +4,21 @@ import { fetchAllHistoryReducer, fetchThreadReducer } from "../reducer/historyRe
 
 
 export const dryRunAction = () => async (dispatch, getState) => {
-    try {
-        const dataToSend = {
-            "configuration":{
-                "model":"gpt-3.5-turbo",
-                "temperature":1,
-                "prompt":[{"system":"hey"}],
-                "type":"chat",
-                  "user":[{"role":"user","content":"hello"}]
-            },
-              
-            "org_id":"124dfgh67ghj",
-            "apikey":"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"
-        }
-      const data = await axios.post(`http://localhost:7072/api/v1/model/playground/chat/completion` , dataToSend );
-      dispatch(dryRun(data.data));
-    } catch (error) {
-      console.error(error);
+  try {
+    const dataToSend = {
+      "configuration": {
+        "model": "gpt-3.5-turbo",
+        "temperature": 1,
+        "prompt": [{ "system": "hey" }],
+        "type": "chat",
+        "user": [{ "role": "user", "content": "hello" }]
+      },
+
+      "org_id": "124dfgh67ghj",
     }
-  };
+    const data = await axios.post(`http://localhost:7072/api/v1/model/playground/chat/completion`, dataToSend);
+    dispatch(dryRun(data.data));
+  } catch (error) {
+    console.error(error);
+  }
+};
