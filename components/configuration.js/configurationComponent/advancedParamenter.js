f208fdf675357bef0c86ec9b593f363eee750202
Naman Jain <96548337+NJ-jain@users.noreply.github.com>
2024-05-24 12:18:15 +0530

    Af100048 (#119)
    * added all bridges , all chatbot and all response for chatbot at layout and intregration at realtime
    
    * embedding bug fixes
    
    * -
    
    * _
    
    * revert changes in chat component
    
    * added condition for intregration reducer
    
    * _
    
    * _
    
    * added conditions for advanced parameter input field and a special condition for response format
    
    ---------
    
    Co-authored-by: Naman Jain <namanjain@Namans-Mac-mini.local>
    Co-authored-by: Parakh Jain <parakhjain@Parakhs-iMac.local>

--- components/configuration.js/configurationComponent/advancedParamenter.js
+++ components/configuration.js/configurationComponent/advancedParamenter.js
@@ -1,45 +1,63 @@
 import { useCustomSelector } from '@/customSelector/customSelector';
 import { updateBridgeAction } from '@/store/action/bridgeAction';
 import { ChevronDown, ChevronUp } from 'lucide-react';
-import React, { useEffect, useState } from 'react'; // Import useState
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 const AdvancedParameters = ({ params, dataToSend: localdata }) => {
-
     const { modelInfoData } = useCustomSelector((state) => ({
         modelInfoData: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.configuration,
     }));
-    const [dataToSend, setDataToSend] = useState(localdata)
-    const [sliderValues, setSliderValues] = useState({});
-    const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
+    const [dataToSend, setDataToSend] = useState(localdata);
+    const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+    const [sliderValues, setSliderValues] = useState({}); // State to hold slider values
 
     useEffect(() => {
-        setDataToSend(localdata)
-    }, [localdata])
+        setDataToSend(localdata);
+    }, [localdata]);
 
     useEffect(() => {
         if (modelInfoData) {
-            const initialSliderValues = Object.entries(modelInfoData)
-                .filter(([key, value]) => value.field === 'slider')
-                .reduce((acc, [key, value]) => ({ ...acc, [key]: value.default }), {});
+            const initialSliderValues = Object.entries(modelInfoData).reduce((acc, [key, value]) => {
+                if (value.field === 'slider') {
+                    acc[key] = value.default;
+                }
+                return acc;
+            }, {});
             setSliderValues(initialSliderValues);
         }
     }, [modelInfoData]);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const handleSliderChange = (e, key) => {
-        handleInputChange(e, key, true); // Call the existing handleInputChange
-        setSliderValues({ ...sliderValues, [key]: e.target.value }); // Update the slider value in state
+        const newValue = Number(e.target.value);
+        setSliderValues({ ...sliderValues, [key]: newValue });
+    };
+
+    const handleSliderBlur = (e, key) => {
+        const newValue = Number(e.target.value);
+
+        let updatedDataToSend = {
+            ...dataToSend,
+            configuration: {
+                ...dataToSend.configuration,
+                [key]: newValue,
+            }
+        };
+
+        setDataToSend(updatedDataToSend);
+        UpdateBridge(updatedDataToSend);
     };
 
     const handleInputChange = (e, key, isSlider = false) => {
         let newValue = e.target.value;
-        let newCheckedValue = e.target.checked
+        let newCheckedValue = e.target.checked;
         if (e.target.type === 'number') {
             newValue = newValue.includes('.') ? parseFloat(newValue) : parseInt(newValue, 10);
         }
+
 
         let updatedDataToSend = {
             ...dataToSend,
@@ -49,100 +67,83 @@ const AdvancedParameters = ({ params, dataToSend: localdata }) => {
             }
         };
         if (key === 'response_format') {
-            const typeValue = newCheckedValue === true ? 'json_object' : newCheckedValue === false ? 'text' : null;
-            if (typeValue) {
-                updatedDataToSend = {
-                    ...updatedDataToSend,
-                    configuration: {
-                        ...updatedDataToSend.configuration,
-                        [key]: {
-                            type: typeValue,
-                        },
-                    },
-                };
-            }
+            updatedDataToSend.configuration.response_format = newCheckedValue ? { type: "json_object" } : { type: "text" };
         }
 
         setDataToSend(updatedDataToSend);
-
-        if (!isSlider) {
-            UpdateBridge(updatedDataToSend);
-        }
+        UpdateBridge(updatedDataToSend);
     };
 
     const UpdateBridge = (currentDataToSend) => {
         dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: { ...currentDataToSend } }));
-    }
+    };
 
     const toggleAccordion = () => {
         setIsAccordionOpen(!isAccordionOpen);
     };
 
     return (
-        <div className="collapse ">
+        <div className="collapse">
             <input type="radio" name="my-accordion-1" onClick={toggleAccordion} />
             <div className="collapse-title p-0 flex items-center justify-start gap-4 text-xl font-medium cursor-pointer" onClick={toggleAccordion}>
                 Advanced Parameters
-                {/* Conditionally render Chevron icons */}
                 {isAccordionOpen ? <ChevronUp /> : <ChevronDown />}
             </div>
             <div className="collapse-content gap-3 flex flex-col p-0">
-                {modelInfoData && Object.entries(modelInfoData || {}).map(([key, value]) => (
+                {modelInfoData && Object.entries(modelInfoData).map(([key, value]) => (
                     key !== 'model' && key !== 'tools' && key !== 'tool_choice' && key !== "stream" &&
-                    <div className={` ${value.field === "boolean" ? "flex justify-between item-center" : ""} w-full`}>
-
+                    <div key={key} className={` ${value.field === "boolean" ? "flex justify-between item-center" : ""} w-full`}>
                         <div className='flex justify-between items-center w-full'>
-                            <p className='capitalize'> {key.replaceAll("_", " ")}</p>
+                            <p className='capitalize'>{key.replaceAll("_", " ")}</p>
                             {value.field === 'slider' && <p>{sliderValues[key]}</p>}
                         </div>
-                        {value.field === "slider" ?
+                        {value.field === "slider" ? (
                             <input
                                 type="range"
-                                min={value?.min}
-                                max={value?.max}
-                                step={value?.step}
-                                defaultValue={value?.default}
+                                min={value.min}
+                                max={value.max}
+                                step={value.step}
+                                value={sliderValues[key] || value.default}
                                 onChange={(e) => handleSliderChange(e, key)}
-                                onBlur={(e) => handleInputChange(e, key)}
+                                onBlur={(e) => handleSliderBlur(e, key)}
                                 className="range range-xs w-full"
                                 name={key}
                                 id={key}
                             />
-                            : value.field === 'text' ?
-                                <input
-                                    type="text"
-                                    required={value?.level === 1 ? true : false}
-                                    defaultValue={typeof value?.default === 'object' ? JSON.stringify(value?.default) : value?.default}
-                                    onBlur={(e) => handleInputChange(e, key)}
-                                    className="input w-full input-bordered max-w-xs  input-sm"
-                                    name={key} // Add name attribute
-                                />
-                                : value.field === 'number' ?
-                                    <input
-                                        type="number"
-                                        required={value?.level === 1 ? true : false}
-                                        defaultValue={value?.default}
-                                        onBlur={(e) => handleInputChange(e, key)}
-                                        className="input w-full input-bordered max-w-xs  input-sm"
-                                        name={key} // Add name attribute
-                                    />
-                                    :
-                                    value.field === 'boolean' ?
-                                        <input
-                                            type="checkbox"
-                                            required={value?.level === 1 ? true : false}
-                                            defaultChecked={value.default.type === "text" ? false : value.default.type === "json_object" ? true : value.default} // Ensure this is a boolean value. Use `!!` to coerce to boolean if necessary.
-                                            onChange={(e) => handleInputChange(e, key)}
-                                            className="checkbox"
-                                            name={key} // Add name attribute
-                                        />
-                                        :
-                                        "this field is under development "}
+                        ) : value.field === 'text' ? (
+                            <input
+                                type="text"
+                                required={value.level === 1}
+                                defaultValue={typeof value.default === 'object' ? JSON.stringify(value.default) : value.default}
+                                onBlur={(e) => handleInputChange(e, key)}
+                                className="input w-full input-bordered max-w-xs input-sm"
+                                name={key}
+                            />
+                        ) : value.field === 'number' ? (
+                            <input
+                                type="number"
+                                required={value.level === 1}
+                                defaultValue={value.default}
+                                onBlur={(e) => handleInputChange(e, key)}
+                                className="input w-full input-bordered max-w-xs input-sm"
+                                name={key}
+                            />
+                        ) : value.field === 'boolean' ? (
+                            <input
+                                type="checkbox"
+                                required={value.level === 1}
+                                defaultChecked={value.default.type === "text" ? false : value.default.type === "json_object" ? true : value.default}
+                                onChange={(e) => handleInputChange(e, key)}
+                                className="checkbox"
+                                name={key}
+                            />
+                        ) : (
+                            "this field is under development"
+                        )}
                     </div>
                 ))}
             </div>
         </div>
-
     );
 };
 
