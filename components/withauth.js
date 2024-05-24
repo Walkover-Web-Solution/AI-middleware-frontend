f7deb1a810445b6885824f0fa1d25d55c5d2e381
harsh <145131947+HARSH-WALKOVER@users.noreply.github.com>
2024-05-24 13:02:56 +0530

    local login implement (#4)
    * local login implement
    
    * resolve comments
    
    * update api '
    gp
    
    ---------
    
    Co-authored-by: harsh sahu <sahupavan000@gmail.com>

--- components/withauth.js
+++ components/withauth.js
@@ -1,25 +1,16 @@
 "use client"
+import { useRouter } from 'next/navigation';
 
+const WithAuth = (Children) => {
+  return (props) => {
+    const router = useRouter();
 
-import { useSearchParams , useRouter } from "next/navigation";
-const  WithAuth =  (Children ) => {
-return (props) => {
-    const router  = useRouter()
-   const searchParams = useSearchParams()
-    const  proxy_auth_token =searchParams.get('proxy_auth_token') 
-
-    if (proxy_auth_token) {
-      localStorage?.setItem('proxy_token', proxy_auth_token);
-      router.replace("/org");
-      return;
+    if (localStorage.getItem('proxy_token')) {
+      router.replace('/org');
     }
-
-    if (localStorage?.getItem('proxy_token')) {
-      router.replace("/org");
-      return;
-    }
-    return <Children /> ;
+    return <Children />;
   };
-}
+};
+
 export default WithAuth;
 
