3bde7ce3deca15eab09a4e53b19fac6355fb94ee
Naman Jain <namanjain@Namans-Mac-mini.local>
2024-06-11 13:39:44 +0530

    added functionlity for update action

--- components/configuration.js/configurationComponent/actionList.js
+++ components/configuration.js/configurationComponent/actionList.js
@@ -1,54 +1,59 @@
 import { useCustomSelector } from '@/customSelector/customSelector';
-import { X } from 'lucide-react';
-import ActionModel from './actionModel';
-import { useDispatch } from 'react-redux';
 import { createOrRemoveActionBridge } from '@/store/action/chatBotAction';
+import { Trash } from 'lucide-react';
+import { useState, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import ActionModel from './actionModel';
 
 export default function ActionList({ params }) {
-
     const { action, bridgeType } = useCustomSelector((state) => ({
         action: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.actions,
         bridgeType: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.bridgeType
     }));
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [selectedKey, setSelectedKey] = useState(null);
 
-
-    const HandleRemoveAction = (actionId, type, description, data) => {
+    const handleRemoveAction = useCallback((actionId, type, description, data, e) => {
+        e.stopPropagation();
         const dataToSend = {
             actionId,
             actionJson: {
                 description,
-                type
+                type,
+                ...(type === 'Frontend' && { data })
             }
         };
 
-        if (type === 'Frontend') {
-            dataToSend.actionJson.data = data;
-        }
-
         dispatch(createOrRemoveActionBridge({
-            orgId: params?.org_id, bridgeId: params?.id, type: "remove", dataToSend
+            orgId: params?.org_id,
+            bridgeId: params?.id,
+            type: "remove",
+            dataToSend
         }));
-        // Add your submit logic here
-    };
+    }, [dispatch, params]);
+
     return (
         bridgeType === "chatbot" && (
             <div className="form-control">
                 <p className='text-xl font-medium'>Action</p>
                 <div className='flex flex-wrap gap-4'>
-                    {action && Object.entries(action).sort().map(([key, value], index) => (
+                    {action && Object.entries(action).sort().map(([key, value]) => (
                         <div
-                            key={index}
-                            className="flex w-[250px] flex-col items-start rounded-md border md:flex-row "
+                            key={key}
+                            className="flex w-[250px] flex-col items-start rounded-md border md:flex-row cursor-pointer"
+                            onClick={() => {
+                                setSelectedKey(key);
+                                document.getElementById('actionModel').showModal();
+                            }}
                         >
                             <div className="p-4 w-full">
                                 <div className='flex items-center justify-between'>
                                     <h1 className="inline-flex items-center text-lg font-semibold">
                                         {key}
                                     </h1>
-                                    <div onClick={() => HandleRemoveAction(key, value.type, value.description, value.data)}>
-                                        <X size={16} color="#ff0000" className='cursor-pointer' />
+                                    <div onClick={(e) => handleRemoveAction(key, value.type, value.description, value.data, e)} className='hover:scale-125 transition duration-100 ease-in-out'>
+                                        <Trash size={16} className='cursor-pointer' />
                                     </div>
                                 </div>
                                 <p className="mt-3 text-xs sm:text-sm text-gray-600 line-clamp-3">
@@ -70,9 +75,8 @@ export default function ActionList({ params }) {
                         </div>
                     ))}
                 </div>
-                <ActionModel params={params} />
+                <ActionModel params={params} actionId={selectedKey} setActionId={setSelectedKey} />
             </div>
         )
     );
-
 }
