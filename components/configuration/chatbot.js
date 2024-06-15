fcbd3052cd8d7ceff9b5c84e735a0c85617a8967
parakhjain23 <parakhjain2301@gmail.com>
2024-06-15 11:41:26 +0530

    chatbot token issue bug fix

--- components/configuration/chatbot.js
+++ components/configuration/chatbot.js
@@ -1,29 +1,27 @@
 import { useCustomSelector } from "@/customSelector/customSelector";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 const Chatbot = ({ params }) => {
 
-    const { bridge } = useCustomSelector((state) => ({
+    const { bridge, chatbot_token } = useCustomSelector((state) => ({
         chatbotData: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.chatbotData,
         bridge: state?.bridgeReducer?.allBridgesMap?.[params?.id],
+        chatbot_token: state?.ChatBot?.chatbot_token || ''
     }));
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            if (window.SendDataToChatbot) {
+            if (window.SendDataToChatbot && window.openChatbot && document.getElementById('parentChatbot')) {
                 clearInterval(intervalId);
-                if (typeof window.SendDataToChatbot === 'function' && document.getElementById('parentChatbot')) {
-                    window.SendDataToChatbot({
-                        bridgeName: bridge?.slugName,
-                        threadId: bridge?.name.replaceAll(" ", ""),
-                        variables: {},
-                        parentId: 'parentChatbot',
-                        fullScreen: true,
-                        hideCloseButton: true
-                    });
-                    window.openChatbot();
-                }
+                window.SendDataToChatbot({
+                    bridgeName: bridge?.slugName,
+                    threadId: bridge?.name.replaceAll(" ", ""),
+                    variables: {},
+                    parentId: 'parentChatbot',
+                    fullScreen: true,
+                    hideCloseButton: true
+                });
+                window.openChatbot();
             }
         }, 100); // Check every 100ms
 
@@ -37,7 +35,7 @@ const Chatbot = ({ params }) => {
                 window.closeChatbot();
             }
         };
-    }, []);
+    }, [chatbot_token]);
 
     return (
         <p>Loading....</p>
