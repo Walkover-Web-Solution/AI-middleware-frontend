b4c94c83f3efcab9608c0cb661465b06a605ecdd
Parakh Jain <97786329+parakhjain23@users.noreply.github.com>
2024-06-17 15:31:31 +0530

    more methods in chatbot (#47)
    * more methods in chatbot
    
    * __

---
+++ components/copyButton/copyButton.tsx
@@ -0,0 +1,36 @@
+import { Copy } from 'lucide-react';
+import React, { useState } from 'react';
+
+const CopyButton = ({data}) => {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboardSendData = () => {
+    // Your clipboard copy logic here
+    navigator.clipboard.writeText(data || '');
+
+    // Show the copied message
+    setCopied(true);
+
+    // Hide the copied message after 2 seconds
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <div>
+      <Copy
+        className="absolute right-5 top-5 cursor-pointer text-white"
+        size={'20px'}
+        onClick={copyToClipboardSendData}
+      />
+      {copied && (
+        <span className="absolute right-5 top-10 text-sm text-green-500">
+          Copied!
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default CopyButton;
