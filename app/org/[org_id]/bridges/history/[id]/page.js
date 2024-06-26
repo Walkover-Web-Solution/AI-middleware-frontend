870aefde0e111a65a8dde1e91fc84e8b1a43b63b
Naman Jain <namanjain@Namans-Mac-mini.local>
2024-06-26 19:42:40 +0530

    remove the function call on click event

--- app/org/[org_id]/bridges/history/[id]/page.js
+++ app/org/[org_id]/bridges/history/[id]/page.js
@@ -194,12 +194,14 @@ function Page({ params }) {
                       {item.role === "tools_call" ?
                         <div className="w-full flex align-center justify-center" >
                           {Object.keys(item.function).map((funcName, index) => (
-                            <div role="alert" className="alert shadow-lg w-2/3 cursor-pointer hover:bg-base-300 transition-colors duration-200" onClick={() => openViasocket(funcName)}>
+                            // <div role="alert" className="alert shadow-lg w-2/3 cursor-pointer hover:bg-base-300 transition-colors duration-200" onClick={() => openViasocket(funcName)}>
+                            <div role="alert" className="alert shadow-lg w-1/3 cursor-pointer hover:bg-base-300 transition-colors duration-200" >
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                               <div>
                                 <h3 className="font-bold">Functions Executed</h3>
                                 <div key={index}>
-                                  <div className="text-xs">Function "{integrationData[funcName].title || funcName}" executed successfully. Inspect details?</div>
+                                  {/* <div className="text-xs">Function "{integrationData[funcName].title || funcName}" executed successfully. Inspect details?</div> */}
+                                  <div className="text-xs">Function "{integrationData[funcName].title || funcName}" executed successfully.</div>
                                 </div>
                               </div>
                             </div>
