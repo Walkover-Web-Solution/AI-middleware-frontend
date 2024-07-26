3a73acbacbac2b2ed056d6c819e21ea8a8ce73f9
ViaSocket-Git <developers@whozzat.com>
2024-07-26 14:03:05 +0530

    find bridge by slugName

--- app/org/[org_id]/bridges/page.js
+++ app/org/[org_id]/bridges/page.js
@@ -116,11 +116,11 @@ function Home({ params }) {
                               {item.name}
                             </h1>
                             <p className="text-xs w-full flex items-center gap-2 line-clamp-5">
+                            <p>SlugName: {item.slugName}</p>
                               {item.configuration?.prompt && (
                                 <>
-                                  {Array.isArray(item.configuration.prompt) ? item.configuration.prompt.map((promptItem, index) => (
+                                {Array.isArray(item.configuration.prompt) ? item.configuration.prompt.map((promptItem, index) => (
                                     <div key={index}>
-                                      <p>SlugName: {item.slugName}</p>
                                       <p>Role: {promptItem.role}</p>
                                       <p>Content: {promptItem.content}</p>
                                     
