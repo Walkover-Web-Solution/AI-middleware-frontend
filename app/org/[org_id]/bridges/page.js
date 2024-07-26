d3d8e662cad3abcc52d50359e29404a038226d0e
Viasocket Interns <developers@whozzat.com>
2024-07-26 19:13:22 +0530

    Af00074 remove duplicate name (#67)
    * search by slugname
    
    * find bridge by slugName
    
    * find by slugName
    
    * findby slugname
    
    * findby Slugname
    
    * removed duplicae name

--- app/org/[org_id]/bridges/page.js
+++ app/org/[org_id]/bridges/page.js
@@ -28,32 +28,16 @@ function Home({ params }) {
   const dispatch = useDispatch();
   const router = useRouter();
   const [searchTerm, setSearchTerm] = useState('');
-
+   console.log(allBridges);
   const filteredBridges = allBridges.filter((item) =>
+   
     item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    item.service.toLowerCase().includes(searchTerm.toLowerCase()) ||
+     item?.slugName?.toLowerCase()?.includes(searchTerm.toLocaleLowerCase()) ||
+    item?.service?.toLowerCase().includes(searchTerm.toLowerCase()) ||
     (item.configuration?.model && item.configuration.model.toLowerCase().includes(searchTerm.toLowerCase())) ||
     item._id.toLowerCase().includes(searchTerm.toLowerCase()) // Add this line
+    
   );
-  // const handleDeleteBridge = async (bridgeId) => {
-  //   // Confirm delete action
-  //   const confirmDelete = window.confirm('Are you sure you want to delete this bridge?');
-
-  //   // If confirmed
-  //   if (confirmDelete) {
-  //     try {
-  //       // Dispatch delete bridge action and get all bridges
-  //       dispatch(deleteBridgeAction({ bridgeId, orgId: params.org_id }));
-  //       toast.success('Bridge deleted successfully');
-  //       dispatch(getAllBridgesAction());
-  //     } catch (error) {
-  //       // Log error
-  //       console.error('Failed to delete bridge:', error);
-  //       // Show toast error
-  //       toast.error('Error deleting bridge');
-  //     }
-  //   }
-  // };
 
   const onClickConfigure = (id) => {
     router.push(`/org/${params.org_id}/bridges/configure/${id}`);
@@ -106,16 +90,18 @@ function Home({ params }) {
                                 : <svg width="24" height="25" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M16 8.016A8.522 8.522 0 008.016 16h-.032A8.521 8.521 0 000 8.016v-.032A8.521 8.521 0 007.984 0h.032A8.522 8.522 0 0016 7.984v.032z" fill="url(#prefix__paint0_radial_980_20147)" /><defs><radialGradient id="prefix__paint0_radial_980_20147" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="matrix(16.1326 5.4553 -43.70045 129.2322 1.588 6.503)"><stop offset=".067" stopColor="#9168C0" /><stop offset=".343" stopColor="#5684D1" /><stop offset=".672" stopColor="#1BA1E3" /></radialGradient></defs></svg>
 
                               }
+                              
                               {item['name']}
-                              {item.name}
                             </h1>
                             <p className="text-xs w-full flex items-center gap-2 line-clamp-5">
+                           {item.slugName ? <p>SlugName: {item.slugName}</p>:""}
                               {item.configuration?.prompt && (
                                 <>
-                                  {Array.isArray(item.configuration.prompt) ? item.configuration.prompt.map((promptItem, index) => (
+                                {Array.isArray(item.configuration.prompt) ? item.configuration.prompt.map((promptItem, index) => (
                                     <div key={index}>
                                       <p>Role: {promptItem.role}</p>
                                       <p>Content: {promptItem.content}</p>
+                                    
                                     </div>
                                   )) : (
                                     <p>Prompt: {item.configuration.prompt}</p>
