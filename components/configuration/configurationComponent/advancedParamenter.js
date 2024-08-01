9114dd47cee29f9a9191fb9802e3d889ea46dcb7
parakhjain23 <parakhjain2301@gmail.com>
2024-08-01 19:51:28 +0530

    showind additional parameters

--- components/configuration/configurationComponent/advancedParamenter.js
+++ components/configuration/configurationComponent/advancedParamenter.js
@@ -5,10 +5,16 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 const AdvancedParameters = ({ params }) => {
-    const { modelInfoData, bridge } = useCustomSelector((state) => ({
-        modelInfoData: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.configuration,
+    const { service, model, type, bridge } = useCustomSelector((state) => ({
+        service: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.service?.toLowerCase(),
+        model: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.configuration?.model,
+        type: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.configuration?.type,
         bridge: state?.bridgeReducer?.allBridgesMap?.[params?.id],
 
+    }));
+    const serviceModel = model?.replace(/-/g, '_');
+    const { modelInfoData } = useCustomSelector((state) => ({
+        modelInfoData: state?.modelReducer?.serviceModels?.[service]?.[type]?.[serviceModel]?.configuration?.additional_parameters,
     }));
 
     const [isAccordionOpen, setIsAccordionOpen] = useState(false);
@@ -80,24 +86,24 @@ const AdvancedParameters = ({ params }) => {
     };
 
     const AdvancedParameters = {
-        creativity_level: { name: 'Creativity Level', fieldtype: 'slider', description: 'Adjusts how creative the responses are' },
-        token_selection_limit: { name: 'Max Tokens Limit', fieldtype: 'slider', description: 'Sets the maximum number of tokens' },
-        topP: { name: 'Top P', fieldtype: 'slider', description: 'Controls the diversity of responses' },
-        json_mode: { name: 'JSON Mode', fieldtype: 'boolean', description: 'Enable or disable JSON format' },
-        probability_cutoff: { name: 'Probability Cutoff', fieldtype: 'slider', description: 'Sets the threshold for probability' },
-        repetition_penalty: { name: 'Repetition Penalty', fieldtype: 'slider', description: 'Reduces repetition in responses' },
-        novelty_penalty: { name: 'Novelty Penalty', fieldtype: 'slider', description: 'Penalizes responses that lack novelty' },
-        log_probability: { name: 'Log Probability', fieldtype: 'boolean', description: 'Log the probabilities of responses' },
-        response_count: { name: 'Response Count', fieldtype: 'number', description: 'Number of responses to generate' },
-        response_suffix: { name: 'Response Suffix', fieldtype: 'text', description: 'Text to add at the end of responses' },
-        stop_sequences: { name: 'Stop Sequences', fieldtype: 'text', description: 'Sequences that signal the end of response' },
-        input_text: { name: 'Input Text', fieldtype: 'text', description: 'The initial input text' },
-        echo_input: { name: 'Echo Input', fieldtype: 'boolean', description: 'Repeat the input text in the response' },
-        best_of: { name: 'Best Of', fieldtype: 'number', description: 'Generate multiple responses and select the best' },
-        seed: { name: 'Seed', fieldtype: 'number', description: 'Set a seed for random number generation' }
+        creativity_level: { name: 'Creativity Level', description: 'Adjusts how creative the responses are' },
+        token_selection_limit: { name: 'Max Tokens Limit', description: 'Sets the maximum number of tokens' },
+        topP: { name: 'Top P', description: 'Controls the diversity of responses' },
+        json_mode: { name: 'JSON Mode', description: 'Enable or disable JSON format' },
+        probability_cutoff: { name: 'Probability Cutoff', description: 'Sets the threshold for probability' },
+        repetition_penalty: { name: 'Repetition Penalty', description: 'Reduces repetition in responses' },
+        novelty_penalty: { name: 'Novelty Penalty', description: 'Penalizes responses that lack novelty' },
+        log_probability: { name: 'Log Probability', description: 'Log the probabilities of responses' },
+        response_count: { name: 'Response Count', description: 'Number of responses to generate' },
+        response_suffix: { name: 'Response Suffix', description: 'Text to add at the end of responses' },
+        stop_sequences: { name: 'Stop Sequences', description: 'Sequences that signal the end of response' },
+        input_text: { name: 'Input Text', description: 'The initial input text' },
+        echo_input: { name: 'Echo Input', description: 'Repeat the input text in the response' },
+        best_of: { name: 'Best Of', description: 'Generate multiple responses and select the best' },
+        seed: { name: 'Seed', description: 'Set a seed for random number generation' }
     };
-    
-    
+
+
     return (
         <div className="collapse text-base-content">
             <input type="radio" name="my-accordion-1" onClick={toggleAccordion} />
@@ -106,63 +112,10 @@ const AdvancedParameters = ({ params }) => {
                 {isAccordionOpen ? <ChevronUp /> : <ChevronDown />}
             </div>
             {isAccordionOpen && <div className="collapse-content gap-3 flex flex-col p-2">
-                {/* {modelInfoData && Object.entries(modelInfoData)?.map(([key, value]) => (
-                    key !== 'model' && key !== 'tools' && key !== 'tool_choice' && key !== "stream" &&
-                    <div key={key} className={` ${value?.field === "boolean" ? "flex justify-between item-center" : ""}`}>
-                        <div className='flex justify-between items-center w-full'>
-                            <p className='capitalize'>{key?.replaceAll("_", " ")}</p>
-                            {value?.field === 'slider' && <p>{sliderValues[key]}</p>}
-                        </div>
-                        {value?.field === "slider" ? (
-                            <input
-                                type="range"
-                                min={value?.min}
-                                max={value?.max}
-                                step={value?.step}
-                                defaultValue={sliderValues[key] || value?.default}
-                                onChange={(e) => handleSliderChange(e, key)}
-                                onBlur={(e) => handleSliderBlur(e, key)}
-                                className="range range-xs w-full"
-                                name={key}
-                                id={key}
-                            />
-                        ) : value?.field === 'text' ? (
-                            <input
-                                type="text"
-                                required={value?.level === 1}
-                                defaultValue={typeof value?.default === 'object' ? JSON.stringify(value?.default) : value?.default}
-                                onBlur={(e) => handleInputChange(e, key)}
-                                className="input w-full input-bordered max-w-xs input-sm"
-                                name={key}
-                            />
-                        ) : value?.field === 'number' ? (
-                            <input
-                                type="number"
-                                required={value?.level === 1}
-                                defaultValue={value?.default}
-                                onBlur={(e) => handleInputChange(e, key)}
-                                className="input input-bordered max-w-xs input-sm"
-                                name={key}
-                            />
-                        ) : value?.field === 'boolean' ? (
-                            <input
-                                type="checkbox"
-                                required={value?.level === 1}
-                                defaultChecked={value?.default?.type === "text" ? false : value?.default?.type === "json_object" ? true : value?.default}
-                                onChange={(e) => handleInputChange(e, key)}
-                                className="checkbox"
-                                name={key}
-                            />
-                        ) : (
-                            "this field is under development"
-                        )}
-                    </div>
-                ))} */}
+                {modelInfoData && Object.entries(modelInfoData).map(([key, { field, min, max, step, default: defaultValue}]) => {
+                    const name = AdvancedParameters[key]?.name || key;
+                    const description = AdvancedParameters[key]?.description || '';
 
-                {modelInfoData && Object.entries(AdvancedParameters).map(([key, { name, fieldtype, description }]) => {
-                    if (!modelInfoData[key]) return null;
-                    const fieldData = modelInfoData[key] || {};
-                    const { min, max, value, default: defaultValue, step } = fieldData;
 
                     return (
                         <div key={key} className="form-control">
@@ -173,9 +126,9 @@ const AdvancedParameters = ({ params }) => {
                                         <Info size={12} />
                                     </div>
                                 </div>
-                                <p className='text-right'>{value}</p>
+                                <p className='text-right'>{bridge?.configuration?.[key]}</p>
                             </label>
-                            {fieldtype === 'slider' && (
+                            {field === 'slider' && (
                                 <div>
                                     <input
                                         type="range"
@@ -183,13 +136,13 @@ const AdvancedParameters = ({ params }) => {
                                         max={max || 100}
                                         step={step || 1}
                                         defaultValue={defaultValue || 0}
-                                        onChange={(e) => handleInputChange(e, key)}
+                                        onBlur={(e) => handleInputChange(e, key)}
                                         className="range range-xs w-full"
                                         name={key}
                                     />
                                 </div>
                             )}
-                            {fieldtype === 'text' && (
+                            {field === 'text' && (
                                 <input
                                     type="text"
                                     defaultValue={defaultValue || ''}
@@ -198,7 +151,7 @@ const AdvancedParameters = ({ params }) => {
                                     name={key}
                                 />
                             )}
-                            {fieldtype === 'number' && (
+                            {field === 'number' && (
                                 <input
                                     type="number"
                                     min={min}
@@ -210,15 +163,7 @@ const AdvancedParameters = ({ params }) => {
                                     name={key}
                                 />
                             )}
-                            {fieldtype === 'boolean' && (
-                                // <input
-                                //     type="checkbox"
-                                //     defaultChecked={defaultValue || false}
-                                //     onChange={(e) => handleInputChange(e, key)}
-                                //     className="checkbox"
-                                //     name={key}
-                                // />
-
+                            {field === 'boolean' && (
                                 <label className='flex items-center justify-start w-fit gap-4 bg-base-100 text-base-content'>
                                     <input
                                         type="checkbox"
