696db53ffac471e2509fc84f9af30613a005b9b0
parakhjain23 <parakhjain2301@gmail.com>
2024-08-14 19:43:18 +0530

    solved prompt input box issue

--- components/configuration/configurationComponent/inputConfigComponent.js
+++ components/configuration/configurationComponent/inputConfigComponent.js
@@ -1,20 +1,30 @@
 import { useCustomSelector } from '@/customSelector/customSelector';
 import { updateBridgeAction } from '@/store/action/bridgeAction';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 const InputConfigComponent = ({ params }) => {
-    const { prompt, service, serviceType } = useCustomSelector((state) => ({
+    const { prompt: reduxPrompt, service, serviceType } = useCustomSelector((state) => ({
         prompt: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.configuration?.prompt || "",
         serviceType: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.configuration?.type || "",
         service: state?.bridgeReducer?.allBridgesMap?.[params?.id]?.service || "",
     }));
+    const [prompt, setPrompt] = useState(reduxPrompt);
     const dispatch = useDispatch();
 
-    const handlePromptChange = (e) => {
+    useEffect(() => {
+        setPrompt(reduxPrompt);
+    }, [reduxPrompt]);
+
+    const savePrompt = useCallback((e) => {
         const newValue = e.target?.value || "";
         dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: { configuration: { prompt: newValue } } }));
-    }
+    },[]);
+
+    const handlePromptChange = useCallback((e) => {
+        const newValue = e.target?.value || "";
+        setPrompt(newValue);
+    },[]);
 
     if (service === "google" && serviceType === "chat") {
         return null;
@@ -26,10 +36,10 @@ const InputConfigComponent = ({ params }) => {
                 <span className="label-text capitalize">Prompt</span>
             </div>
             <textarea
-                key={prompt}
                 className="textarea textarea-bordered border w-full min-h-96 resize-y focus:border-primary"
-                defaultValue={prompt}
-                onBlur={handlePromptChange}
+                value={prompt}
+                onChange={handlePromptChange}
+                onBlur={savePrompt}
             ></textarea>
         </div>
     );
