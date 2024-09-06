1c61d1abecf7912b7083e4570ba31bb5fc7600dd
ViaSocket-Git <abbassadri786@gmail.com>
2024-09-06 18:25:36 +0530

    History-Function_Log

--- components/historyPageComponents/threadItem.js
+++ components/historyPageComponents/threadItem.js
@@ -1,17 +1,66 @@
+'use client'
 import { Bot, Info, User } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "../codeBlock/codeBlock";
+import { useCustomSelector } from "@/customSelector/customSelector";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
-const ThreadItem = ({ item, threadHandler, formatDateAndTime, integrationData }) => (
+const ThreadItem = ({ item, threadHandler, formatDateAndTime, integrationData }) => {
+  const pathName = usePathname();
+  // dispatch = useDispatch();
+  const path = pathName?.split('?')[0].split('/');
+  const orgId = path[2] || '';
+  const {  embedToken } = useCustomSelector((state) => ({
+   
+    embedToken: state?.bridgeReducer?.org?.[orgId]?.embed_token,
+  
+  }));
+  useEffect(() => {
+    if (embedToken) {
+      const script = document.createElement("script");
+      script.setAttribute("embedToken", embedToken);
+      script.id = process.env.NEXT_PUBLIC_EMBED_SCRIPT_ID;
+      script.src = process.env.NEXT_PUBLIC_EMBED_SCRIPT_SRC;
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(document.getElementById(process.env.NEXT_PUBLIC_EMBED_SCRIPT_ID));
+      };
+    }
+  }, [embedToken]);
+
+  useEffect(() => {
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
+
+  function handleMessage(e) {
+    // todo: need to make api call to update the name & description
+    if (e?.data?.webhookurl) {
+      const dataToSend = {
+        ...e.data,
+        status: e?.data?.action
+      }
+      }
+    }
+  
+
+  
+  return(
   <div key={`item.id${item.id}`} >
     {item.role === "tools_call" ? (
       <div className="w-full flex overflow-y-scroll align-center justify-center gap-2">
         {Object.keys(item.function).map((funcName, index) => (
           <div key={index} role="alert" className="alert w-1/3 transition-colors duration-200">
             <Info size={16} />
-            <div>
+            <div onClick={() => openViasocket(funcName, {flowHitId:'funcName'})} className="cursor-pointer">
               <h3 className="font-bold">Functions Executed</h3>
-              <div className="text-xs">Function "{integrationData?.[funcName]?.title || funcName}" executed successfully.</div>
+              <div  className="text-xs">Function "{integrationData?.[funcName]?.title || funcName}" executed successfully.</div>
             </div>
           </div>
         ))}
@@ -45,6 +94,6 @@ const ThreadItem = ({ item, threadHandler, formatDateAndTime, integrationData })
       </div>
     )}
   </div>
-);
+);}
 
 export default ThreadItem;
