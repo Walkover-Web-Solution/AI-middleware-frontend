e51429f4e821f33b454529c82add65ac258b6662
Viasocket Interns <developers@whozzat.com>
2024-09-07 16:02:34 +0530

    pauthkey route bug fixes prod (#112)
    * pauth key route bug fixes
    
    * remove chatbot and metrics from navbar

--- components/navbar.js
+++ components/navbar.js
@@ -74,12 +74,12 @@ function Navbar() {
           <Building2 size={16} /> {organizations[path[2]]?.name}
         </button>
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn capitalize m-1">{path[3] === 'apikey' ? 'API Key' : path[3]} <ChevronDown size={16} /></div>
-          <ul tabIndex={0} className="dropdown-content z-[100] menu p-2 shadow bg-base-100 rounded-box w-52">
-            {['bridges', "chatbot", 'pauthkey','apikeys', 'metrics', 'invite'].map((item) => (
-              <li key={item} onClick={() => router.push(`/org/${path[2]}/${item}`)}>
+          <div tabIndex={0} role="button" className="btn capitalize m-1">{path[3] === 'apikeys' ? 'API Keys' : path[3]} <ChevronDown size={16} /></div>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            {['bridges', 'pauthkey','apikeys','invite'].map((item) => (
+                <li key={item} onClick={() => router.push(`/org/${path[2]}/${item}`)}>
                 <a className={path[3] === item ? "active" : ""}>{item.charAt(0).toUpperCase() + item.slice(1)}</a>
-              </li>
+               </li>
             ))}
           </ul>
         </div>
