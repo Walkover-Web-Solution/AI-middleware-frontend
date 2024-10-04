84406f9ce58623d44717c773ba50892f9cee08f8
ViaSocket-Git <developers@whozzat.com>
2024-10-04 14:31:02 +0530

    history function ui

--- components/historyPageComponents/threadItem.js
+++ components/historyPageComponents/threadItem.js
@@ -1,4 +1,4 @@
-import { Bot, MessageCircleCode, Pencil, User, Info, FileClock, SquareFunction, Parentheses} from "lucide-react";
+import { Bot, MessageCircleCode, Pencil, User, Info, FileClock, SquareFunction, Parentheses } from "lucide-react";
 import React, { useRef, useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import { useDispatch } from "react-redux";
@@ -21,7 +21,7 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
   useEffect(() => {
     setMessageType(item?.updated_message ? 2 : item?.chatbot_message ? 0 : 1);
   }, [item]);
-  
+
 
   const handleToggle = () => {
     setShowNormalMessage((prevState) => !prevState);
@@ -67,9 +67,9 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
 
   const getMessageToDisplay = () => {
     if (messageType === 0) return item.chatbot_message;
-    if(messageType===1)return  item.content;
+    if (messageType===1)return  item.content;
     if (messageType === 2) return item.updated_message;
-    
+
   };
 
   const selectMessageType = (type) => {
@@ -106,33 +106,38 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
       toolsDataModalRef.current.close();
     }
   };
+  // Truncate function
+function truncate(string, maxLength) {
+  if (string.length > maxLength) {
+      return string.substring(0, maxLength - 3) + '...'; 
+  }
+  return string;
+}
 
   return (
     <div key={`item-id-${item?.id}`} >
       {item?.role === "tools_call" ? (
         <div className="mb-2">
-          <h1 className="mt-4 auto bg-base-200 p-2 w-1/4 mb-2 opacity-80 rounded-md flex items-center gap-2">
+          <h1 className="mt-4 auto p-2 w-1/4 mb-2 opacity-80 rounded-md flex items-center gap-2">
             <span><Parentheses size={16} /></span> Functions Executed Successfully
           </h1>
           <div className="w-full flex overflow-x-auto h-full gap-1 justify-start items-center whitespace-nowrap">
             {Object.entries(item.tools_call_data[0] || {}).map(([key, tool], index) => (
-              <div key={index} className="bg-base-200 rounded-lg w-1/4 flex gap-2 duration-200 items-center justify-between hover:bg-base-300">
-                <div
-                  onClick={() => openViasocket(tool.name, { flowHitId: JSON.parse(item.function[tool.name])?.metadata?.flowHitId })}
-                  className="cursor-pointer flex items-center justify-center pl-2"
-                >
+              <div key={index} className="bg-base-200 rounded-lg w-1/7  flex gap-4 duration-200 items-center justify-between hover:bg-base-300">
+                <div onClick={() => openViasocket(tool.name, { flowHitId: JSON.parse(item.function[tool.name])?.metadata?.flowHitId })}
+                  className="cursor-pointer flex items-center justify-center py-4 pl-2">
                   <div className="text-xs font-bold text-center">
-                    {integrationData?.[tool.name]?.title || tool.name}
+                    {truncate(integrationData?.[tool.name]?.title || tool.name, 20)}
                   </div>
                 </div>
                 <div className="flex gap-3">
 
-                  <div className="tooltip tooltip-left py-4 relative" data-tip="function logs">
+                  <div className="tooltip tooltip-left  relative" data-tip="function logs">
                     <SquareFunction size={18}
                       onClick={() => openViasocket(tool.name, { flowHitId: JSON.parse(item.function[tool.name])?.metadata?.flowHitId })}
                       className="opacity-80 cursor-pointer" />
                   </div>
-                  <div className="tooltip tooltip-left py-4 pr-2 relative" data-tip="function data">
+                  <div className="tooltip tooltip-left  pr-2 relative" data-tip="function data">
                     <FileClock
                       size={18}
                       onClick={() => {
@@ -156,25 +161,25 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
                   {item.role === "user" ? (
                     <User size={20} />
                   ) : (
-                   <div> 
-                   <Bot
-                      className=" cursor-pointer bot-icon"
-                      size={20}
-                      onClick={() => setIsDropupOpen(!isDropupOpen)}
-                    /></div>
+                    <div>
+                      <Bot
+                        className=" cursor-pointer bot-icon"
+                        size={20}
+                        onClick={() => setIsDropupOpen(!isDropupOpen)}
+                      /></div>
                   )}
                 </div>
                 {isDropupOpen && item.role !== "user" && (
                   <div
                     ref={dropupRef}
                     className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 bg-inherit rounded-md shadow-lg "
-                    style={{ zIndex:9 }}
+                    style={{ zIndex: 9 }}
                   >
                     <ul className="flex justify-center flex-col items-center gap-2">
                       {item.chatbot_message && (
                         <li className={`${item.chatbot_message ? "" : "hidden"}`}>
                           <button
-                           className={`px-2 py-1 ${messageType === 0 ? "bg-primary text-white rounded-md" : ""
+                            className={`px-2 py-1 ${messageType === 0 ? "bg-primary text-white rounded-md" : ""
                               }`}
                             onClick={() => selectMessageType(0)}
                           >
@@ -213,10 +218,10 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
                 )}
               </div>
             </div>
-            <div className="chat-header flex gap-4 items-center mb-1">    
-            {messageType===2 ? <p className="text-xs opacity-50">Edited</p> : ""}
+            <div className="chat-header flex gap-4 items-center mb-1">
+              {messageType === 2 ? <p className="text-xs opacity-50">Edited</p> : ""}
             </div>
-            <div className={`${item.role === "user" ? "cursor-pointer chat-bubble-primary " : "bg-base-200  text-base-content pr-10"} chat-bubble transition-all ease-in-out duration-300`} onClick={() => threadHandler(item.thread_id, item)}>       
+            <div className={`${item.role === "user" ? "cursor-pointer chat-bubble-primary " : "bg-base-200  text-base-content pr-10"} chat-bubble transition-all ease-in-out duration-300`} onClick={() => threadHandler(item.thread_id, item)}>
               <ReactMarkdown components={{
                 code: ({ node, inline, className, children, ...props }) => (
                   <CodeBlock
@@ -228,7 +233,7 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
                   </CodeBlock>
                 )
               }}>
-                  {getMessageToDisplay()}
+                {getMessageToDisplay()}
               </ReactMarkdown>
               {item?.role === 'assistant' && (
                 <div className="tooltip absolute top-2  right-2 text-sm cursor-pointer" data-tip="Edit response">
@@ -242,12 +247,12 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
             {item?.role === "assistant" && <time className="text-xs opacity-50 chat-end">{formatDateAndTime(item.createdAt)}</time>}
           </div>
           {item.role == 'assistant' && item.is_reset && <div class="flex justify-center items-center my-4">
-                <p class="border-t border-base-300 w-full"></p>
-                <p class="bg-error text-base-100 py-2 px-4 rounded-md mx-4 whitespace-nowrap">
-                  History cleared
-                </p>
-                <p class="border-t border-base-300 w-full"></p>
-              </div>}
+            <p class="border-t border-base-300 w-full"></p>
+            <p class="bg-error text-base-100 py-2 px-4 rounded-md mx-4 whitespace-nowrap">
+              History cleared
+            </p>
+            <p class="border-t border-base-300 w-full"></p>
+          </div>}
           {
             item?.error && (
               <div className="chat chat-end">
@@ -256,12 +261,12 @@ const ThreadItem = ({ index, item, threadHandler, formatDateAndTime, integration
                   <p>{item.error}</p>
                 </div>
                 <div className="w-100 p-3 rounded-full bg-base-300 flex justify-center items-center">
-                  <Bot size={20}/>
+                  <Bot size={20} />
                 </div>
               </div>
             )
           }
-          
+
         </div>
       )}
 
