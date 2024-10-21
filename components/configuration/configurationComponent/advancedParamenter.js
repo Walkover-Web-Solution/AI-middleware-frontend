4aee6658526a9ad3145d02b2e8c3fcecbd576b43
parakhjain23 <parakhjain2301@gmail.com>
2024-10-21 16:26:04 +0530

    only update values when necessary

--- components/configuration/configurationComponent/advancedParamenter.js
+++ components/configuration/configurationComponent/advancedParamenter.js
@@ -32,7 +32,9 @@ const AdvancedParameters = ({ params }) => {
                 [key]: isSlider ? Number(newValue) : e.target.type === 'checkbox' ? newCheckedValue : newValue,
             }
         };
-        dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: { ...updatedDataToSend } }));
+        if ((isSlider ? Number(newValue) : e.target.type === 'checkbox' ? newCheckedValue : newValue) !== configuration[key]) {
+            dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: { ...updatedDataToSend } }));
+        }
     };
 
     const handleSelectChange = (e, key) => {
@@ -47,7 +49,9 @@ const AdvancedParameters = ({ params }) => {
                 [key]: newValue,
             }
         };
-        dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: { ...updatedDataToSend } }));
+        if (newValue !== configuration[key]) {
+            dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: { ...updatedDataToSend } }));
+        }
     };
 
     const toggleAccordion = () => {
@@ -60,13 +64,14 @@ const AdvancedParameters = ({ params }) => {
                 [key]: value
             }
         };
-        if(value !== configuration[key])
-        dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: updatedDataToSend }));
-
+        if (value !== configuration[key]) {
+            dispatch(updateBridgeAction({ bridgeId: params.id, dataToSend: updatedDataToSend }));
+        }
     };
+
     return (
         <div className="collapse text-base-content" tabIndex={0}>
-            <input type="radio" name="my-accordion-1" onClick={toggleAccordion} className='cursor-pointer'/>
+            <input type="radio" name="my-accordion-1" onClick={toggleAccordion} className='cursor-pointer' />
             <div className="collapse-title p-0 flex items-center justify-start font-medium cursor-pointer" onClick={toggleAccordion}>
                 <span className="mr-2 cursor-pointer">
                     Advanced Parameters
@@ -74,7 +79,7 @@ const AdvancedParameters = ({ params }) => {
 
                 {isAccordionOpen ? <ChevronUp /> : <ChevronDown />}
             </div>
-            
+
             {isAccordionOpen && <div className="collapse-content gap-3 flex flex-col p-3 border rounded-md">
                 {modelInfoData && Object.entries(modelInfoData || {})?.map(([key, { field, min, max, step, default: defaultValue, options }]) => {
                     if (KEYS_NOT_TO_DISPLAY.includes(key)) return null;
@@ -85,12 +90,12 @@ const AdvancedParameters = ({ params }) => {
                         <div key={key} className="form-control">
                             <label className="label">
                                 <div className='flex gap-2'>
-                                <div className='flex flex-row gap-2 items-center'>
-                                    <span className="label-text capitalize">{name || key}</span>
-                                    {description && <div className="tooltip tooltip-right" data-tip={description}>
-                                        <Info size={12} />
-                                    </div>}
-                                </div>
+                                    <div className='flex flex-row gap-2 items-center'>
+                                        <span className="label-text capitalize">{name || key}</span>
+                                        {description && <div className="tooltip tooltip-right" data-tip={description}>
+                                            <Info size={12} />
+                                        </div>}
+                                    </div>
                                     <div>
                                         <ul className="menu menu-xs menu-horizontal lg:menu-horizontal bg-base-200 p-1 rounded-md text-xs">
                                             {field === 'slider' && (<li><a onClick={() => setSliderValue("min", key)} className={configuration?.[key] === "min" ? 'bg-base-content text-base-100' : ''}>Min</a></li>)}
@@ -102,7 +107,7 @@ const AdvancedParameters = ({ params }) => {
 
                                 </div>
                                 {((field === 'slider') && !(min <= configuration?.[key] && configuration?.[key] <= max)) && (configuration?.['key']?.type === "string") && (error = true)}
-                                {field === 'slider' && <p className={`text-right ${error ? 'text-error' : ''}`} id={`sliderValue-${key}`}>{(configuration?.[key] === 'min' || configuration?.[key] === 'max' || configuration?.[key]==='default') ?
+                                {field === 'slider' && <p className={`text-right ${error ? 'text-error' : ''}`} id={`sliderValue-${key}`}>{(configuration?.[key] === 'min' || configuration?.[key] === 'max' || configuration?.[key] === 'default') ?
                                     modelInfoData?.[key]?.[configuration?.[key]] : configuration?.[key]}</p>}
                             </label>
                             {field === 'slider' && (
@@ -114,8 +119,8 @@ const AdvancedParameters = ({ params }) => {
                                         step={step || 1}
                                         key={configuration?.[key]}
                                         defaultValue={
-                                            (configuration?.[key] === 'min' || configuration?.[key] === 'max' || configuration?.[key]=='default') ?
-                                                modelInfoData?.[key]?.[configuration?.[key]]:
+                                            (configuration?.[key] === 'min' || configuration?.[key] === 'max' || configuration?.[key] == 'default') ?
+                                                modelInfoData?.[key]?.[configuration?.[key]] :
                                                 configuration?.[key]
                                         }
                                         onBlur={(e) => handleInputChange(e, key, true)}
@@ -130,7 +135,7 @@ const AdvancedParameters = ({ params }) => {
                             {field === 'text' && (
                                 <input
                                     type="text"
-                                    defaultValue={configuration?.[key] ==='default' ? '' : configuration?.[key] || ''}
+                                    defaultValue={configuration?.[key] === 'default' ? '' : configuration?.[key] || ''}
                                     onBlur={(e) => handleInputChange(e, key)}
                                     className="input input-bordered input-sm w-full"
                                     name={key}
