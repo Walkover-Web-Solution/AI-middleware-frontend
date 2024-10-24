ab59f7580474767271573f53df898ed59ca29d6f
parakhjain23 <parakhjain2301@gmail.com>
2024-10-24 12:32:32 +0530

    archieve unarchive feature

--- config/index.js
+++ config/index.js
@@ -583,3 +583,13 @@ export const updateFunctionApi = async ({ function_id, dataToSend }) => {
     throw new Error(error);
   }
 };
+
+export const archiveBridgeApi = async (bridge_id) => {
+  try {
+    const response = await axios.put(`${URL}/api/v1/config/bridge-status/${bridge_id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
