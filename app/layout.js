6efe2b2944cb5f0f09abd6d555fa4b6c823bb4c2
parakhjain23 <parakhjain2301@gmail.com>
2024-10-25 10:43:50 +0530

    added ssecurity header

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
 
     <html lang="en" data-theme="light">
-
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body suppressHydrationWarning>
         <Wrapper>
           {children}
