50cc1606f289d5406982a3c988d3cf5ef8422d7e
Parakh Jain <97786329+parakhjain23@users.noreply.github.com>
2024-10-26 11:44:05 +0530

    json format guide for function calling (#221)

---
+++ app/faq/jsonformatdoc/page.js
@@ -0,0 +1,21 @@
+'use server';
+import { JSONFORMATDATA } from '@/utils/enums';
+import React from 'react';
+
+const JsonFomatDocGuide = async () => {
+    return (
+        <div className="max-w-[80%] mx-auto p-6 relative">
+            <div>
+                <h1 className="text-2xl font-bold text-center mb-6">JSON Format Structure for Function Calling</h1>
+                {JSONFORMATDATA.map(({ title, json }) => (
+                    <div key={title} className="faq-section mb-6 p-4 border border-gray-300 rounded-lg">
+                        <h3 className="text-lg font-semibold">{title}</h3>
+                        <pre className="bg-gray-100 p-3 rounded mt-2">{json}</pre>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default JsonFomatDocGuide;
