6b1afe4a83e6736f2347d0fa37e985d9ceff0d0a
parakhjain23 <parakhjain2301@gmail.com>
2024-11-29 18:13:42 +0530

    ..

--- next.config.js
+++ next.config.js
@@ -3,12 +3,18 @@ module.exports = {
     missingSuspenseWithCSRBailout: false,
   },
   reactStrictMode: false,
-  webpack(config, { isServer }) {
-    // Exclude async_hooks from client-side bundle
+  webpack: (config, { isServer }) => {
     if (!isServer) {
+      config.node = {
+        ...config.node,
+        fs: 'empty',  // Mock the 'fs' module
+        net: 'empty',  // Mock the 'net' module
+        tls: 'empty',  // Mock the 'tls' module
+        async_hooks: 'empty',  // Mock the 'async_hooks' module
+      };
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        async_hooks: false,
+        async_hooks: false, // Mock async_hooks for the browser
       };
     }
     return config;
