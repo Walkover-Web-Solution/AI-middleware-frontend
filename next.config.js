0961599b44e96e72ccd839e6597e07d2c54735c4
harsh sahu <harshksahu11@gmail.com>
2024-12-05 13:01:30 +0530

    _

--- next.config.js
+++ next.config.js
@@ -3,20 +3,4 @@ module.exports = {
     missingSuspenseWithCSRBailout: false,
   },
   reactStrictMode: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        ...config.node,
-        fs: 'empty',  // Mock the 'fs' module
-        net: 'empty',  // Mock the 'net' module
-        tls: 'empty',  // Mock the 'tls' module
-        async_hooks: 'empty',  // Mock the 'async_hooks' module
-      };
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        async_hooks: false, // Mock async_hooks for the browser
-      };
-    }
-    return config;
-  },
-};
+}
