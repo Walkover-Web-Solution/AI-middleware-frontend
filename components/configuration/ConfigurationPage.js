da93e751f007e96c630e5a69d3dfe922ed202f17
Viasocket Interns <developers@whozzat.com>
2024-12-13 13:34:56 +0530

    image model config  main (#250)
    * image model config
    
    * image history added in history page
    
    ---------
    
    Co-authored-by: Natwar589 <natwaarrathor16082003@gmail.com>

--- components/configuration/ConfigurationPage.js
+++ components/configuration/ConfigurationPage.js
@@ -63,18 +63,18 @@ export default function ConfigurationPage({ params }) {
                 currentView === 'setup' ?
                     <>
                         {bridgeType === 'chatbot' && <SlugNameInput params={params} />}
-                        <PreEmbedList params={params} />
-                        <InputConfigComponent params={params} />
+                        {modelType !== "image" && <PreEmbedList params={params} />}
+                        {modelType !== 'image' && <InputConfigComponent params={params} />}
                         {(modelType == 'chat' || modelType == 'fine-tune') && <EmbedList params={params} />}
                         <ServiceDropdown params={params} />
                         <ModelDropdown params={params} />
                         <ApiKeyInput params={params} />
                         <AdvancedParameters params={params} />
-                        <AddVariable params={params} />
-                        <GptMemory params={params} />
-                        {bridgeType === "chatbot" && <UserRefernceForRichText params={params} />}
-                        <ActionList params={params} />
-                        {bridgeType === 'api' && <ResponseFormatSelector params={params} />}
+                        {modelType !== "image"  && <AddVariable params={params} />}
+                        {modelType !== 'image' && <GptMemory params={params} /> }
+                        {bridgeType === "chatbot" && modelType !== 'image' &&  <UserRefernceForRichText params={params} />}
+                        { modelType !== 'image' && <ActionList params={params} />}
+                        {bridgeType === 'api' && modelType !== 'image' && <ResponseFormatSelector params={params} />}
                     </>
                     :
                     bridgeType === 'api' ?
