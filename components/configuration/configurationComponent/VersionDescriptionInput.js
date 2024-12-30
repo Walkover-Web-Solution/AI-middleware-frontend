ef0f1bfe03a804130e1fd87a38053a664d34d496
Natwar589 <natwaarrathor16082003@gmail.com>
2024-12-30 11:13:33 +0530

    donot dispatch on both new and redux values are same

--- components/configuration/configurationComponent/VersionDescriptionInput.js
+++ components/configuration/configurationComponent/VersionDescriptionInput.js
@@ -15,10 +15,12 @@ function VersionDescriptionInput({ params }) {
     const inputRef = useRef(null);
 
     const saveBridgeVersionDescription = useCallback(() => {
-        const newValue = inputRef.current?.value || "";
-        dispatch(updateBridgeVersionAction({ versionId: params?.version, dataToSend: { version_description: newValue } }));
         setIsEditable(false);
-    }, [dispatch, params?.version]);
+        const newValue = inputRef.current?.value.trim() || "";
+        if (newValue === versionDescription) 
+        return;
+        dispatch(updateBridgeVersionAction({ versionId: params?.version, dataToSend: { version_description: newValue } }));
+    }, [dispatch, params?.version, versionDescription]);
 
     const handleKeyDown = useCallback((e) => {
         if (e.key === 'Enter') {
@@ -28,15 +30,17 @@ function VersionDescriptionInput({ params }) {
 
     return (
         <div className='mb-2'>
-            <div className='flex flex-col items-start gap-2'>
-                <p className='font-bold'>Bridge Version Description : </p>
+            <div className='flex flex-row items-center gap-2'>
+                <div className="label">
+                    <span className="label-text capitalize font-medium">Version Description :</span>
+                </div>
                 {!isEditable && <div className='flex items-center gap-4'>
                     <div className="flex items-center gap-4">
-                        <div className="bg-base">{versionDescription || <span className='opacity-50'>Enter your bridge version description</span>}</div>
+                        <div className="bg-base">{versionDescription || <span className='opacity-50'>Enter version description</span>}</div>
                         <Pencil size={16} onClick={() => setIsEditable(true)} className='' />
                     </div>
                 </div>}
-                {isEditable && <input ref={inputRef} type="text" placeholder="Enter your bridge version description" autoFocus className="input w-full max-w-xs text-lg bg-base placeholder-opacity-50" onBlur={saveBridgeVersionDescription} defaultValue={versionDescription} onKeyDown={handleKeyDown} />}
+                {isEditable && <input ref={inputRef} type="text" placeholder="Enter version description" autoFocus className="input w-full max-w-xs bg-base placeholder-opacity-50" onBlur={saveBridgeVersionDescription} defaultValue={versionDescription} onKeyDown={handleKeyDown} />}
             </div>
         </div>
     )
