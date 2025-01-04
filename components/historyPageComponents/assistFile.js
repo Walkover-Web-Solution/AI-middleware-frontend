4125c6b2be04a3ff91dd4db3ed8eb6f29517547e
Natwar589 <natwaarrathor16082003@gmail.com>
2025-01-04 12:41:04 +0530

    add turncate function in assist file

--- components/historyPageComponents/assistFile.js
+++ components/historyPageComponents/assistFile.js
@@ -31,3 +31,10 @@ export const useHandleClickOutside = (sidebarRef, setIsSliderOpen) => {
     };
   }, [sidebarRef, setIsSliderOpen]);
 };
+
+export const truncate = (string = "", maxLength) => {
+  return string.length > maxLength
+    ? `${string.substring(0, maxLength - 3)}...`
+    : string;
+};
+
