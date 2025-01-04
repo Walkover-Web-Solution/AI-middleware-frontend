d8ae92a0e0585a27f5f73e43b6c6407bdba11b1c
Natwar589 <natwaarrathor16082003@gmail.com>
2025-01-04 12:33:44 +0530

    show the variables in modals main

--- components/modals/ChatAiConfigDeatilViewModal.js
+++ components/modals/ChatAiConfigDeatilViewModal.js
@@ -6,7 +6,7 @@ import React from 'react'
 const ChatAiConfigDeatilViewModal = ({modalContent}) => {
   return (
     <dialog id="chat_details_view" className="modal">
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[1000] overflow-auto min-w-[100vw]">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[1000] overflow-auto min-w-[100vw] h-auto">
         <div className="bg-white p-4 rounded shadow-lg max-w-6xl w-[90vw] relative">
           <button
             className="absolute top-6 right-5"
@@ -15,7 +15,7 @@ const ChatAiConfigDeatilViewModal = ({modalContent}) => {
             <CircleX size={24} />
           </button>
           <h3 className="text-lg font-semibold mb-2 p-2">Detailed View</h3>
-          <div className="bg-gray-200 p-4 rounded text-sm overflow-auto h-[80vh] ">
+          <div className="bg-gray-200 p-4 rounded text-sm overflow-auto h-auto max-h-[80vh] ">
             {Object.entries(modalContent || {}).map(([key, value]) => (
               <div key={key} className="mb-2">
                 <strong className="capitalize">{key}:</strong>
