4fc29b3ef71c755b4e06905017b3c510126cb93f
krish736 <112505529+krish736@users.noreply.github.com>
2025-01-25 17:13:12 +0530

    landing page update/ remove login button

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,17 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useLayoutEffect, useState } from "react";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useLayoutEffect(() => {
+    const proxyToken = localStorage.getItem("proxy_token");
+    if (proxyToken) {
+      setIsLoggedIn(true);
+    }
+  }, []);
   return (
     <header className="flex justify-between items-center px-3 md:px-28 py-8">
       <div className="flex space-x-12">
@@ -14,26 +24,32 @@ const Header = () => {
               width={32}
               height={32}
             />
-            <p className=" font-medium cursor-pointer">AI MIDDLEWARE</p>
+            <p className="font-medium cursor-pointer">AI MIDDLEWARE</p>
           </div>
         </Link>
-        {/* <p className=" font-medium cursor-pointer">Pricing</p> */}
       </div>
 
       <div className="flex gap-4">
-        <Link href="/login">
-          <button className="btn px-6 py-2 bg-transparent border border-white text-white rounded hover:text-black">
-            Login
-          </button>
-        </Link>
-        {/* <button className="px-6 py-2 bg-custom-blue text-white rounded ">
-          Start for free
-        </button> */}
-        <Link href="/login">
-          <button role="button" className="btn btn-primary">
-            Start for free
-          </button>
-        </Link>
+        {isLoggedIn ? (
+          <Link href="/login">
+            <button className="btn px-6 py-2 bg-transparent border border-white text-white rounded hover:text-black">
+              Go to App
+            </button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <button className="btn px-6 py-2 bg-transparent border border-white text-white rounded hover:text-black">
+              Login
+            </button>
+          </Link>
+        )}
+        {!isLoggedIn && (
+          <Link href="/login">
+            <button role="button" className="btn btn-primary">
+              Start for free
+            </button>
+          </Link>
+        )}
       </div>
     </header>
   );
