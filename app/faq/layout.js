d2ce0e34e2955194de32c709701ccadd1de38e6d
Viasocket Interns <developers@whozzat.com>
2025-01-30 18:04:38 +0530

    sitemap and metdata (#341)

---
+++ app/faq/layout.js
@@ -0,0 +1,18 @@
+export const metadata = {
+    title: "faq",
+    description: "Find answers for all your questions relates to AI Middleware right here",
+};
+
+
+
+export default function RootLayout({ children }) {
+    return (
+
+        <html lang="en" data-theme="light">
+
+            <body suppressHydrationWarning>
+                {children}
+            </body>
+        </html>
+    );
+}
