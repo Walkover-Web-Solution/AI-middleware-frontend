4c0853fb89da481489d3024a549cf976e048c29d
Natwar589 <natwaarrathor16082003@gmail.com>
2025-02-10 11:26:09 +0530

    sentry monitring testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,59 @@
-module.exports = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  {
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+    reactStrictMode: false,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      if (process.env.NEXT_PUBLIC_ENV === 'production') {
+        config.plugins.push(
+          new webpack.DefinePlugin({
+            'process.env.SENTRY_DSN': JSON.stringify('https://3e380f336fb01167df53aa4eba59923e@o4508736624459776.ingest.us.sentry.io/4508737844477952'),
+          }),
+        );
+      }
+      return config;
+    },
   },
-  reactStrictMode: false,
-}
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    org: "walkover-gz",
+    project: "ai-middleware",
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Automatically annotate React components to show their full name in breadcrumbs and session replay
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+
+    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    // tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  }
+);
