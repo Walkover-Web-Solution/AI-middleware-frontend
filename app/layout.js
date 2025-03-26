689494a90c47cc411cd477279b66d19b06fc8714
parakhjain23 <parakhjain2301@gmail.com>
2025-03-26 10:57:20 +0530

    change metadata

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import Wrapper from "@/wrapper/Wrapper";
 const inter = Syne({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "GTWY AI",
+  title: "GTWY AI | Connect 5000+ apps in just 1 click",
   description: "Simplified AI & chatbot integration",
   keywords:
     "ai middleware, ai integration platform, ai chatbot service, openai integration, anthropic api, groq ai, o1 ai, ai automation tools, ai api gateway, large language model integration, llm api, ai software solutions, ai-powered chatbot, ai model deployment, machine learning api, enterprise ai solutions, ai infrastructure, artificial intelligence services, custom ai development, ai orchestration, ai cloud services, multi-ai platform, ai business solutions, ai developer tools, ai framework, gpt integration, ai tools for business, llm deployment, ai model hosting, ai tech stack, ai-powered applications, smart ai assistant, best ai middleware, chatbot development platform, ai-powered automation",
