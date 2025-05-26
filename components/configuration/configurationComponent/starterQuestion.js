10fddf8b4c24c40e237e97cffe083d006c043b0f
Viasocket Interns <developers@whozzat.com>
2025-05-26 12:39:33 +0530

    added staterquestion toggle button (#570)
    * added staterquestion button
    
    * changed the key name

---
+++ components/configuration/configurationComponent/starterQuestion.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useCustomSelector } from '@/customHooks/customSelector';
+import { updateBridgeVersionAction } from '@/store/action/bridgeAction';
+import { useDispatch } from 'react-redux';
+import { Info } from 'lucide-react';
+
+const StarterQuestionToggle = ({ params }) => {
+    const dispatch = useDispatch();
+    const IsstarterQuestionEnable = useCustomSelector((state) => 
+        state?.bridgeReducer?.bridgeVersionMapping?.[params?.id]?.[params?.version]?.IsstarterQuestionEnable || false
+    );
+
+    const handleToggle = () => {
+        dispatch(updateBridgeVersionAction({
+            bridgeId: params.id,
+            versionId: params.version,
+            dataToSend: { IsstarterQuestionEnable: !IsstarterQuestionEnable }
+        }));
+    };
+
+    return (
+        <div className="flex items-center gap-2">
+            <div className="label cursor-pointer">
+                <span className="font-medium mr-2">Starter Question</span>
+                <div className="tooltip" data-tip="Toggle to enable/disable starter questions">
+                    <Info size={14} className="inline-block" />
+                </div>
+            </div>
+            <input
+                type="checkbox"
+                checked={IsstarterQuestionEnable}
+                onChange={handleToggle}
+                className="toggle toggle-primary"
+                defaultValue={IsstarterQuestionEnable ? "true" : "false"}
+            />
+        </div>
+    );
+};
+
+export default StarterQuestionToggle;
