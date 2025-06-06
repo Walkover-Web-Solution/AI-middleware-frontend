b14e0c276afa32c7c407a7ad7ffaa58b351f1acf
Natwar589 <natwarrathor961@gmail.com>
2025-06-06 17:03:55 +0530

    public agent

--- components/chatbotConfiguration/chatbotGuide.js
+++ components/chatbotConfiguration/chatbotGuide.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react'
 import PrivateFormSection from './firstStep'
 import FormSection from './formSection'
 import SecondStep from './secondStep'
+import PublicAgentForm from './PublicAgentForm';
 
 function ChatbotGuide({ params }) {
     const [chatbotId, setChatBotId] = useState("");
     return (
         <>
-            <PrivateFormSection params={params} ChooseChatbot={true} setChatBotIdFucntion={setChatBotId}/>
-            <FormSection params={params} chatbotId={chatbotId}/>
-            <SecondStep />
+                <div className="bg-base-200 p-4 rounded-md ">
+                   <h1>Public Chatbot configuration</h1>
+                   <PublicAgentForm params={params}/>
+                </div>
+                <div className="divider"></div>
+                <div className="">
+                    <PrivateFormSection params={params} ChooseChatbot={true} setChatBotIdFucntion={setChatBotId}/>
+                    <FormSection params={params} chatbotId={chatbotId}/>
+                    <SecondStep />
+                </div>
         </>
+
     )
 }
 
