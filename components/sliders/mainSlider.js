299654279d203715f05c4bb05a3ad00c7e22a7e0
Yogesh Patel <ygurjar932@gmail.com>
2025-06-12 11:51:03 +0530

    tutorialmodal

--- components/sliders/mainSlider.js
+++ components/sliders/mainSlider.js
@@ -1,9 +1,11 @@
 import { logoutUserFromMsg91 } from '@/config';
 import { useCustomSelector } from '@/customHooks/customSelector';
-import { toggleSidebar } from '@/utils/utility';
-import { AlignJustify, BookText, Bot, Building2, ChevronDown, Cog, Key, KeyRound, LineChart, LogOut, Mail, MessageSquareMore, PlugZap, Settings2, TriangleAlert, UserPlus } from 'lucide-react';
+import { closeModal, openModal, toggleSidebar } from '@/utils/utility';
+import { AlignJustify, BookText, Bot, Building2, ChevronDown, Cog, Key, KeyRound, LineChart, LogOut, Mail, MessageSquareMore, PlugZap, Settings2, TriangleAlert, UserPlus, BookCheck, MonitorPlay } from 'lucide-react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
+import TutorialModal from '@/components/modals/tutorialModal';
+import { MODAL_TYPE } from '@/utils/enums';
 
 function MainSlider() {
   const pathName = usePathname();
@@ -26,7 +28,12 @@ function MainSlider() {
     metrics: <LineChart />,
     knowledge_base: <BookText />
   }
-
+ const handleOpenModal=()=>{
+      openModal(MODAL_TYPE.TUTORIAL_MODAL)
+ }
+ const handleCloseModal=()=>[
+        closeModal(MODAL_TYPE.TUTORIAL_MODAL)
+ ]
   const logoutHandler = async () => {
     try {
       await logoutUserFromMsg91({
@@ -65,88 +72,91 @@ function MainSlider() {
   };
 
   return (
-    <div className="relative">
-      <label htmlFor="my-drawer-2" className="drawer-button lg:hidden z-0 absolute top-3 left-1">
-        <AlignJustify size={24} onClick={toggleMainSidebar} />
-      </label> <div className={`drawer lg:drawer-open relative z-[101] lg:z-0`}>
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <div className="flex flex-col h-screen w-72 bg-base-100 border-r py-4">
-            <div className="flex-grow overflow-y-auto px-4">
-              <div className='mb-4 flex-col gap-2'>
-                <h2 className="text-xl font-bold text-start">
-                  {organizations[path[2]]?.name.length > 15
-                    ? organizations[path[2]]?.name.substring(0, 15) + '...'
-                    : organizations[path[2]]?.name}
-                </h2>
-                <button
-                  type="button"
-                  onClick={handleSwitchOrganization}
-                  className="text-sm text-blue-500 hover:underline z-[102] bg-transparent border-none p-0 cursor-pointer focus:outline-none"
-                >
-                  Switch Organization
-                </button>
+    <>
+      <div className="relative">
+        <label htmlFor="my-drawer-2" className="drawer-button lg:hidden z-0 absolute top-3 left-1">
+          <AlignJustify size={24} onClick={toggleMainSidebar} />
+        </label>
+        <div className={`drawer lg:drawer-open relative z-[101] lg:z-0`}>
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col"></div>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+            <div className="flex flex-col h-screen w-72 bg-base-100 border-r py-4">
+              <div className="flex-grow overflow-y-auto px-4">
+                <div className='mb-4 flex-col gap-2'>
+                  <h2 className="text-xl font-bold text-start">
+                    {organizations[path[2]]?.name.length > 15
+                      ? organizations[path[2]]?.name.substring(0, 15) + '...'
+                      : organizations[path[2]]?.name}
+                  </h2>
+                  <button
+                    type="button"
+                    onClick={handleSwitchOrganization}
+                    className="text-sm text-blue-500 hover:underline z-[102] bg-transparent border-none p-0 cursor-pointer focus:outline-none"
+                  >
+                    Switch Organization
+                  </button>
+                </div>
+                <ul className="menu space-y-2 p-0">
+                  {
+                    ['agents', 'pauthkey', 'apikeys', 'alerts', 'knowledge_base', 'invite', 'metrics'].map((item) => (
+                      <li key={item} onClick={() => router.push(`/org/${path[2]}/${item}`)} className="transition-transform transform hover:scale-105 flex items-center">
+                        <a className={` w-full font-medium ${path[3] === item ? "active text-primary" : "text-gray-700"} `}>
+                          {Icons[item]}
+                          {item === 'knowledge_base' ? 'Knowledge base' : item.charAt(0).toUpperCase() + item.slice(1)}
+                        </a>
+                      </li>
+                    ))}
+                </ul>
               </div>
-              <ul className="menu space-y-2 p-0">
-                {
-                  ['agents', 'pauthkey', 'apikeys', 'alerts', 'knowledge_base', 'invite', 'metrics'].map((item) => (
-                    <li key={item} onClick={() => router.push(`/org/${path[2]}/${item}`)} className="transition-transform transform hover:scale-105 flex items-center">
-                      <a className={` w-full font-medium ${path[3] === item ? "active text-primary" : "text-gray-700"} `}>
-                        {Icons[item]}
-                        {item === 'knowledge_base' ? 'Knowledge base' : item.charAt(0).toUpperCase() + item.slice(1)}
-                      </a>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <div className='mt-auto'>
-              <a
-                href="https://gtwy.featurebase.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-sm font-medium "
-              >
+              <div className='mt-auto'>
                 <div className="mt-4 rounded-lg border-t border-base-300 bg-base-100">
-                  <div className="p-4 flex items-center gap-2">
-                    <MessageSquareMore size={16} />
-                    Feedback
+                  <div className="pb-2 pl-4 pt-4 h-10 flex text-center items-center gap-2 cursor-pointer" onClick={handleOpenModal}>
+                    <MonitorPlay size={16} />
+                    Tutorial
                   </div>
                 </div>
-              </a>
-              <details
-                className="overflow-hidden rounded-lg border-t border-gray-300 [&_summary::-webkit-details-marker]:hidden"
-              >
-                <summary
-                  className="flex cursor-pointer items-center justify-between gap-2 bg-white p-4 text-gray-900 transition"
+                <a
+                  href="https://gtwy.featurebase.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-sm font-medium "
                 >
-                  <span className="text-sm font-medium flex justify-center items-center gap-2"> <Settings2 size={16} /> Setting </span>
-
-                  <span className="transition group-open:-rotate-180">
-                    <ChevronDown strokeWidth={1.25} />
-                  </span>
-                </summary>
-
-                <div className="border-t border-gray-200 bg-white">
-                  <ul className="menu w-full   text-base-content">
-                    <li> <a className='py-2 px-2 rounded-md'> <Mail size={16} /> {userdetails.email}</a> </li>
-                    <li> <a className={`py-2 px-2 rounded-md`} onClick={() => { router.push(`/org/${path[2]}/userDetails`) }}> <Cog size={16} />Update User Details</a> </li>
-                    <li> <a className={`py-2 px-2 rounded-md`} onClick={() => { router.push(`/org/${path[2]}/workspaceSetting`) }}> <Cog size={16} /> Workspace Setting</a> </li>
-                    <li ><a className='py-2 px-2 rounded-md' onClick={logoutHandler}> <LogOut size={16} />  logout</a></li>
-                  </ul>
-                </div>
-                
-              </details>
-              
-             
+                  <div className="mt-4 rounded-lg border-t border-base-300 bg-base-100">
+                    <div className="p-4 flex items-center gap-2">
+                      <MessageSquareMore size={16} />
+                      Feedback
+                    </div>
+                  </div>
+                </a>
+                <details
+                  className="overflow-hidden rounded-lg border-t border-gray-300 [&_summary::-webkit-details-marker]:hidden"
+                >
+                  <summary
+                    className="flex cursor-pointer items-center justify-between gap-2 bg-white p-4 text-gray-900 transition"
+                  >
+                    <span className="text-sm font-medium flex justify-center items-center gap-2"> <Settings2 size={16} /> Setting </span>
+                    <span className="transition group-open:-rotate-180">
+                      <ChevronDown strokeWidth={1.25} />
+                    </span>
+                  </summary>
+                  <div className="border-t border-gray-200 bg-white">
+                    <ul className="menu w-full text-base-content">
+                      <li> <a className='py-2 px-2 rounded-md'> <Mail size={16} /> {userdetails.email}</a> </li>
+                      <li> <a className={`py-2 px-2 rounded-md`} onClick={() => { router.push(`/org/${path[2]}/userDetails`) }}> <Cog size={16} />Update User Details</a> </li>
+                      <li> <a className={`py-2 px-2 rounded-md`} onClick={() => { router.push(`/org/${path[2]}/workspaceSetting`) }}> <Cog size={16} /> Workspace Setting</a> </li>
+                      <li ><a className='py-2 px-2 rounded-md' onClick={logoutHandler}> <LogOut size={16} />  logout</a></li>
+                    </ul>
+                  </div>
+                </details>
+              </div>
             </div>
-          </div >
-        </div >
-      </div >
-    </div >
+          </div>
+        </div>
+      </div>
+       <TutorialModal handleCloseModal={handleCloseModal}/>
+    </>
   );
 }
 
