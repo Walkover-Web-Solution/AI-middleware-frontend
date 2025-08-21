9b60ad11ed0e0af1d6dd6b108ba94e4c12956dcd
Yogesh Patel <ygurjar932@gmail.com>
2025-08-21 18:54:26 +0530

    senity

--- config/index.js
+++ config/index.js
@@ -756,7 +756,7 @@ export const updateUser = async ({ user_id, user }) => {
   const updateObject = { user_id, user: {"meta": user?.meta} };
   try {
     const response = await axios.put(`${URL}/user/updateDetails`, updateObject);
-    return response.data;
+    return response?.data;
   } catch (error) {
     console.error('Error updating details:', error.response?.data?.message || error.message);
     throw new Error(error.response?.data?.message || 'Something went wrong');
@@ -774,7 +774,7 @@ export const updateOrganizationData = async (orgId, orgDetails) => {
         'reference-id': NEXT_PUBLIC_REFERENCEID
       }
     });
-    return response.data;
+    return response?.data;
   } catch (error) {
 
     toast.error('Error updating organization:', error);
