390edc403d8ba645bcd7f956f4cfd9066526753b
Natwar589 <natwarrathor961@gmail.com>
2025-08-22 12:26:06 +0530

    add new component for external link anchor tags

---
+++ components/smartLink.js
@@ -0,0 +1,16 @@
+import Protected from "./protected";
+
+const SmartLink = ({ href, children, isEmbedUser }) => {
+  return (
+    <a
+      href={href}
+      target={isEmbedUser ? "" : "_blank"} // inside iframe → same frame, normal → new tab
+      rel={!isEmbedUser ? "noopener noreferrer" : undefined}
+    >
+      {children}
+    </a>
+  );
+}   
+
+export default Protected(SmartLink);
+
