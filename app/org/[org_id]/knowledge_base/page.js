52490a39a7237f03131d9922574f5926f2bacd3e
Yogesh Patel <ygurjar932@gmail.com>
2025-09-09 16:08:53 +0530

    knowledgeBase delete in grid view fixed

--- app/org/[org_id]/knowledge_base/page.js
+++ app/org/[org_id]/knowledge_base/page.js
@@ -149,7 +149,7 @@ const Page = ({ params }) => {
                       <EllipsisVerticalIcon size={16} />
                     </div>
                     <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box z-high w-32">
-                      <li><a onClick={() => handleDelete()} className="text-error hover:bg-error hover:text-error-content">Delete</a></li>
+                      <li><a onClick={() => handleDeleteKnowledgebase(item)} className="text-error hover:bg-error hover:text-error-content">Delete</a></li>
                       <li><a onClick={() => handleUpdateKnowledgeBase(item)} className="hover:bg-base-200">Update</a></li>
                     </ul>
                   </div>
