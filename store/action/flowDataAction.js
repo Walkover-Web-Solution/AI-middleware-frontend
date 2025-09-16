9f0036b4e2f70212a9e9444f53145eb73e333a27
Viasocket Interns <developers@whozzat.com>
2025-09-16 11:41:13 +0300

    Gruardrail main (#923)
    * guardrail option added
    
    * removed console
    
    * guardrail prod pr
    
    ---------
    
    Co-authored-by: Yogesh Patel <ygurjar932@gmail.com>

--- store/action/flowDataAction.js
+++ store/action/flowDataAction.js
@@ -1,5 +1,5 @@
-import { getApiKeyGuide, getTutorial } from "@/config";
-import { getApiKeyGuideData, getTutorialData } from "../reducer/flowDataReducer";
+import { getApiKeyGuide, getGuardrailsTemplates, getTutorial } from "@/config";
+import { getApiKeyGuideData, getGuardrailsTemplatesData, getTutorialData } from "../reducer/flowDataReducer";
 
 export const getTutorialDataAction = () => {
   return async (dispatch) => {
@@ -17,6 +17,16 @@ export const getApiKeyGuideAction = () => {
     try {
       const data = await getApiKeyGuide();
       dispatch(getApiKeyGuideData(data.data));
+    } catch (error) {
+      console.error("Failed to fetch tutorial data:", error);
+    }
+  };
+};
+export const getGuardrailsTemplatesAction = () => {
+  return async (dispatch) => {
+    try {
+      const data = await getGuardrailsTemplates();
+      dispatch(getGuardrailsTemplatesData(data?.data?.SafetyChecksFunction));
     } catch (error) {
       console.error("Failed to fetch tutorial data:", error);
     }
