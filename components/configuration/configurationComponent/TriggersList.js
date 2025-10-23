1f5844fe049e0e7e912ce78fa493e3fcec738fdb
kishan31082003 <savita.singh5477@gmail.com>
2025-10-23 15:11:35 +0530

    when trigger delted, i filter from frontend as well as delete from the redux store too

--- components/configuration/configurationComponent/TriggersList.js
+++ components/configuration/configurationComponent/TriggersList.js
@@ -1,6 +1,6 @@
 import { getOrCreateNotificationAuthKey } from "@/config";
 import { useCustomSelector } from "@/customHooks/customSelector";
-import { updateTriggerDataReducer } from "@/store/reducer/bridgeReducer";
+import { updateTriggerDataReducer, removeTriggerDataReducer } from "@/store/reducer/bridgeReducer";
 import { AddIcon } from "@/components/Icons";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
@@ -39,7 +39,9 @@ export default function TriggersList({ params, isEmbedUser}) {
     }
     useEffect(() => {
         if (triggerData) {
-            const filteredTriggers = triggerData.filter(flow => flow?.metadata?.bridge_id === params?.id) || []
+            const filteredTriggers = triggerData.filter(flow => 
+                flow?.metadata?.bridge_id === params?.id && flow?.status !== 'deleted'
+            ) || []
             setTriggers(filteredTriggers);
             if (!filteredTriggers?.length && window?.openViasocket && authkey) openTrigger()
         }
@@ -85,14 +87,24 @@ export default function TriggersList({ params, isEmbedUser}) {
             const existingIndex = prevTriggers.findIndex(trigger => trigger.id === newTrigger.id);
 
             if (existingIndex !== -1) {
-                // Update existing trigger
-                const updatedTriggers = [...prevTriggers];
-                updatedTriggers[existingIndex] = { ...prevTriggers[existingIndex], ...newTrigger };
-                return updatedTriggers;
+                // If trigger is deleted, remove it permanently from the array and Redux store
+                if (newTrigger.status === 'deleted') {
+                    dispatch(removeTriggerDataReducer({ triggerId: newTrigger.id, orgId: params?.org_id }));
+                    const updatedTriggers = prevTriggers.filter(trigger => trigger.id !== newTrigger.id);
+                    return updatedTriggers;
+                } else {
+                    // Update existing trigger
+                    const updatedTriggers = [...prevTriggers];
+                    updatedTriggers[existingIndex] = { ...prevTriggers[existingIndex], ...newTrigger };
+                    return updatedTriggers;
+                }
             } else {
-                // Add new trigger to the beginning
-                dispatch(updateTriggerDataReducer({ dataToSend: newTrigger, orgId: params?.org_id }));
-                return [newTrigger, ...prevTriggers];
+                // Add new trigger to the beginning (only if not deleted)
+                if (newTrigger.status !== 'deleted') {
+                    dispatch(updateTriggerDataReducer({ dataToSend: newTrigger, orgId: params?.org_id }));
+                    return [newTrigger, ...prevTriggers];
+                }
+                return prevTriggers;
             }
         });
     }
@@ -122,7 +134,7 @@ export default function TriggersList({ params, isEmbedUser}) {
                 
                 </div>
             <div className="flex flex-wrap gap-4">
-                {triggers?.length ? (triggers?.filter(trigger => trigger?.status !== 'deleted')?.length ? (triggers?.filter(trigger => trigger?.status !== 'deleted')?.map(trigger => {
+                {triggers?.length ? (triggers?.map(trigger => {
                     return (
                         <div key={trigger?.id} onClick={() => { openTrigger(trigger?.id) }}
                             className="group flex h-full p-2 w-full flex-col items-start rounded-md border border-base-300 md:flex-row cursor-pointer bg-base-100 relative hover:bg-base-200 transition-colors duration-200">
@@ -140,7 +152,7 @@ export default function TriggersList({ params, isEmbedUser}) {
                             </div>
                         </div>
                     )
-                })) : null) : null}
+                })) : null}
             </div>
         </div>
     )
