429a671c36c56b0958aa7ef4c0481df8a0a814e2
kishan31082003 <savita.singh5477@gmail.com>
2025-11-01 17:37:03 +0530

    qa reqs fixed

--- components/configuration/configurationComponent/DefaultVariablesSection.js
+++ components/configuration/configurationComponent/DefaultVariablesSection.js
@@ -5,10 +5,10 @@ import InfoTooltip from '@/components/InfoTooltip';
 // Optimized default variables section
 const DefaultVariablesSection = memo(() => {
     return (
-        <div className="bg-gradient-to-r bg-base-1 border-t-0 border border-base-300 rounded-t-none p-2">
+        <div className="bg-gradient-to-r bg-base-1 border-t-0 border border-base-content/10 rounded-t-none p-2 ">
             <div className="flex items-center gap-2">
                 <span className="text-nowrap text-xs font-medium">Default Variables</span>
-                <p role="alert" className="label-text-alt alert p-2 bg-base-200 inline-block">
+                <p role="alert" className="label-text-alt p-2 bg-base-300 inline-block w-fit">
                     &#123;&#123;current_time_and_date&#125;&#125;,
                     &#123;&#123;pre_function&#125;&#125;,
                     &#123;&#123;timezone&#125;&#125;
