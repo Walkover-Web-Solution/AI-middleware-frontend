5ea1c13345147bbdd7ef28bc0fb1a72bb8d5e542
Natwar589 <natwarrathor961@gmail.com>
2025-11-26 11:57:35 +0530

    chore: update Google Tag Manager container ID
    - Changed GTM container ID from GTM-PCV9RTTL to GTM-PXRN8T45
    - Updated both script and noscript implementations
    - Refined noscript comment for consistency

--- app/layout.js
+++ app/layout.js
@@ -31,14 +31,14 @@ export default async function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PCV9RTTL');
+            })(window,document,'script','dataLayer','GTM-PXRN8T45');
           `}
         </Script>
 
-        {/* GTM NoScript Fallback */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PCV9RTTL"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PXRN8T45"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
